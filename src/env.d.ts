7576f3fb10f884ee990fdc0b03e42f9bb9a17eec
M4kU <80482255+Sir-M4kU@users.noreply.github.com>
2024-04-12 10:53:30 -0500

    Add auth integration

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,13 +2,20 @@
 import type { R2Bucket, D1Database } from "@cloudflare/workers-types";
 import type { Runtime } from "@astrojs/cloudflare";
 
+interface ImportMetaEnv {
+	readonly GITHUB_CLIENT_ID: string;
+	readonly GITHUB_CLIENT_SECRET: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
+
 export interface Env {
 	BUCKET: R2Bucket;
 	DB: D1Database;
 }
 
-declare global {
-	namespace App {
-		interface Locals extends Runtime<Env> {}
-	}
+declare namespace App {
+	interface Locals extends Runtime<Env> {}
 }
